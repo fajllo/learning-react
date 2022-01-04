b4672241601c28eb3a0c19230d25678c0441bf0e
fsdr <filipsordyl8@gmail.com>
2022-01-04 21:39:14 +0100

    added button and on click events

--- src/Game.js
+++ src/Game.js
@@ -4,11 +4,19 @@ class Game extends Component {
   state = {
     score: 0,
   };
-
+  handleClick = e => {
+    let score = this.state.score + 1;
+    this.setState({ score });
+  };
   render() {
     return (
       <div className="Game text-4xl">
         <h1>Your score is : {this.state.score}</h1>
+        {this.state.score === 7 ? (
+          <h2> you win</h2>
+        ) : (
+          <button onClick={this.handleClick}>Click me</button>
+        )}
       </div>
     );
   }
