b7cac724da72f5c4c91387085022dae728505ac4
fsdr <filipsordyl8@gmail.com>
2022-01-04 20:20:46 +0100

    initializing state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
+import Game from "./Game";
 
 function App() {
   return (
     <div>
-      <h1 className="text-red-300 text-4xl"> hello</h1>
+      <h1 className="text-red-300 text-4xl"> Game</h1>
+      <Game />
     </div>
   );
 }
---
+++ src/Game.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+    };
+  }
+  render() {
+    return (
+      <div className="Game text-4xl">
+        <h1>Your score is : {this.state.score}</h1>
+      </div>
+    );
+  }
+}
+
+export default Game;
