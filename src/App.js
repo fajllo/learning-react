033f3343370cbc557a5feaeb82fd5fdc3af65871
fsdr <filipsordyl8@gmail.com>
2022-01-11 13:54:42 +0100

    initial code, 3 components cell board app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Board from "./Board";
 function App() {
   return (
-    <div>
-      <h1 className="text-red-300 text-4xl"> hello</h1>
+    <div className="App flex w-screen flex-col items-center bg-gray-700 min-h-screen">
+      <div className=" mt-10 flex flex-col items-center">
+        <h1 className="mb-9">
+          <span className="text-orange-600 text-6xl"> lights </span>
+          <span className="text-6xl text-violet-600">out </span>
+        </h1>
+        <Board />
+      </div>
     </div>
   );
 }
