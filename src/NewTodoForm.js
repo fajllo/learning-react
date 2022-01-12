c8152bce6b654c336fcdb10d4b9a99f7805ca5e4
fsdr <filipsordyl8@gmail.com>
2022-01-12 16:18:09 +0100

    basic app structure

---
+++ src/NewTodoForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewTodoForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
